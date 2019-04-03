4bfd870d204a43ae9e145ea76e4a86460c2d66e8
Dani Arnaiz <daniarnaizg@gmail.com>
2019-04-03 16:41:38 +0200

    Added links to buy/sell from poe.trade
    Also get the league name from poe.ninja API

--- js/poeNinjaAPI.js
+++ js/poeNinjaAPI.js
@@ -1,4 +1,58 @@
-// date = new Date().toJSON().slice(0,10).replace(/-/g,'-');
+
+let leagueName;
+$.getJSON("https://cors-anywhere.herokuapp.com/https://api.poe.watch/leagues", function (data) {
+	leagueName = data[3]['name'];
+	console.log(leagueName)
+});
+
+var currDict = {};
+currDict["Orb of Alteration"] = 1;
+currDict["Orb of Fusing"] = 2;
+currDict["Orb of Alchemy"] = 3;
+currDict["Chaos Orb"] = 4;
+currDict["Gemcutter's Prism"] = 5;
+currDict["Exalted Orb"] = 6;
+currDict["Chromatic Orb"] = 7;
+currDict["Jeweller's Orb"] = 8;
+currDict["Orb of Chance"] = 9;
+currDict["Cartographer's Chisel"] = 10;
+currDict["Orb of Scouring"] = 11;
+currDict["Blessed Orb"] = 12;
+currDict["Orb of Regret"] = 13;
+currDict["Regal Orb"] = 14;
+currDict["Divine Orb"] = 15;
+currDict["Vaal Orb"] = 16;
+currDict["Glassblower's Bauble"] = 21;
+currDict["Orb of Transmutation"] = 22;
+currDict["Orb of Augmentation"] = 23;
+currDict["Mirror of Kalandra"] = 24;
+currDict["Eternal Orb"] = 25;
+currDict["Perandus Coin"] = 26;
+currDict["Silver Coin"] = 35;
+currDict["Apprentice Cartographer's Sextant"] = 45;
+currDict["Journeyman Cartographer's Sextant"] = 46;
+currDict["Master Cartographer's Sextant"] = 47;
+currDict["Splinter of Xoph"] = 52;
+currDict["Splinter of Tul"] = 53;
+currDict["Splinter of Esh"] = 54;
+currDict["Splinter of Uul-Netol"] = 55;
+currDict["Splinter of Chayula"] = 56;
+currDict["Blessing of Xoph"] = 57;
+currDict["Blessing of Tul"] = 58;
+currDict["Blessing of Esh"] = 59;
+currDict["Blessing of Uul-Netol"] = 60;
+currDict["Blessing of Chayula"] = 61;
+currDict["Orb of Annulment"] = 513;
+currDict["Orb of Binding"] = 514;
+currDict["Orb of Horizons"] = 515;
+currDict["Harbinger's Orb"] = 516;
+currDict["Engineer's Orb"] = 517;
+currDict["Ancient Orb"] = 518;
+currDict["Annulment Shard"] = 519;
+currDict["Mirror Shard"] = 520;
+currDict["Exalted Shard"] = 521;
+currDict["Bestiary Orb"] = 630;
+currDict["Harbinger's Shard"] = 643;
 
 function Comparator(a, b) {
 	if (a[4] < b[4]) return 1;
@@ -7,15 +61,12 @@ function Comparator(a, b) {
 }
 
 $.getJSON('https://cors-anywhere.herokuapp.com/https://poe.ninja/api/data/GetCurrencyOverview?league=Synthesis', function (data) {
-	// $.getJSON('https://cors-escape.herokuapp.com/https://poe.ninja/api/data/GetCurrencyOverview?league=Synthesis', function(data) {
 
 	var exPrice = Math.round(data['lines'][3]['chaosEquivalent']);
 	var exIcon = data['currencyDetails'][1]['icon'];
 	var cIcon = data['currencyDetails'][0]['icon'];
 
-	// document.getElementById('exalted').innerHTML = "<span id='ex-bp'>1 <img src=" + exIcon + " alt='icon'> &#8594 " + exPrice + "<img src=" + cIcon + " alt='icon'></span>";
 	document.getElementById('exalted').innerHTML = "<tr id='title'><th id='exName'><img src=" + exIcon + " alt='icon'>Exalted Orb - </th><th id='exValue'>" + exPrice + " chaos</th></tr>"
-
 
 	document.getElementById('currency').innerHTML = "<tr><td id='ex'>&nbsp</td><td id='bp'>PRICE</td><td id='ratio'>RATIO</td><td id='profit'>PROFIT/EX</td></tr>"
 
@@ -55,14 +106,13 @@ $.getJSON('https://cors-anywhere.herokuapp.com/https://poe.ninja/api/data/GetCur
 		}
 
 		currList.push(tempArray)
-		// document.getElementById('currency').innerHTML += "<tr id=" + id + "><th id='name'><img src=" + icon_index + " alt='icon'> " + name + "</th><th id='bp'>" + bp + "</th><th id='ratio'>" + ratio.toFixed(2) + "</th><th id='profit'>" + ppex.toFixed(2) + "</th></tr>"
 	}
 
 
 	currList = currList.sort(Comparator);
 
 	for (var i = 0; i < currList.length; i++) {
-		document.getElementById('currency').innerHTML += "<tr id=" + currList[i][5] + "><th id='name'><img src=" + currList[i][0] + " alt='icon'> " + currList[i][1] + "</th><th id='bp'>" + currList[i][2] + "</th><th id='ratio'>" + currList[i][3].toFixed(2) + "</th><th id='profit'>" + currList[i][4].toFixed(2) + "</th></tr>"
+		document.getElementById('currency').innerHTML += "<tr id=" + currList[i][5] + "><th id='name'><img src=" + currList[i][0] + " alt='icon'> " + currList[i][1] + "<a id='buy-button' href='http://currency.poe.trade/search?league=" + leagueName + "&online=x&want=" + 6 + "&have=" + currDict[currList[i][1]] + "'target='_blank'>BUY</a><a id='sell-button' href='http://currency.poe.trade/search?league=" + leagueName + "&online=x&want=" + currDict[currList[i][1]] + "&have=" + 4 + "'target='_blank'>SELL</a></th><th id='bp'>" + currList[i][2] + "</th><th id='ratio'>" + currList[i][3].toFixed(2) + "</th><th id='profit'>" + currList[i][4].toFixed(2) + "</th></tr>"
 	}
 
 });
